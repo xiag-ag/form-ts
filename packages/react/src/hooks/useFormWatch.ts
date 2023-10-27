a074ba54c9b62fa7a6b9e17f197c9cee05b76667
Alexander Evtushenko <aevtushenko@xiag.ch>
2023-10-27 15:44:33 +0700

    Use `useSyncExternalStore` in `useFormWatch` hook

--- packages/react/src/hooks/useFormWatch.ts
+++ packages/react/src/hooks/useFormWatch.ts
@@ -1,19 +1,11 @@
 import { FormController, FormState } from '@form-ts/core';
-import { useEffect, useState } from 'react';
+import { useCallback, useSyncExternalStore } from 'react';
 
 export function useFormWatch<TData, TError, TValue>(
     form: FormController<TData, TError>,
     selector: (state: FormState<TData, TError>) => TValue,
 ): TValue {
-    const [result, setResult] = useState(() => selector(form.currentState));
-
-    useEffect(() => {
-        setResult(selector(form.currentState));
-
-        return form.subscribe(() => {
-            setResult(selector(form.currentState));
-        });
-    }, [form, selector]);
-
-    return result;
+    const getter = useCallback(() => selector(form.currentState), [form, selector]);
+    const subscribe = useCallback((cb: () => void) => form.subscribe(cb), [form]);
+    return useSyncExternalStore(subscribe, getter);
 }
